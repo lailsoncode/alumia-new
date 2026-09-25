d4e70e21682fedbdb512f9616f29c200f29cc477
Lailson Herondino <lailsonmsa@gmail.com>
2026-09-25 11:36:48 -0300

    feat: adicionar identidade visual ao favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,6 +46,11 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: "stylesheet", href: appCss },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png" },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/icons/apple-touch-icon.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
